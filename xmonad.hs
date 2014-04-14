ed2bc2b3ee205288f9f9b790df8dc0b0a0799b15
Andrei Vladescu-Olt <andrei.volt@gmail.com>
2014-04-14 09:41:52 +0200

    remove unused function

--- xmonad.hs
+++ xmonad.hs
@@ -21,11 +21,6 @@ import XMonad.Hooks.UrgencyHook
 
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-spawnToWorkspace :: String -> String -> X ()
-spawnToWorkspace workspace program = do
-                                     spawn program
-                                     windows $ W.greedyView workspace
-
 -- Send applications to their dedicated Workspace
 myManageHook = composeAll
                 [
